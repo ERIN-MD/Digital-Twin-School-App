1e25b74eaa066a21b99547502c4aa42eae1494fd
Erin-MD <144317574+ERIN-MD@users.noreply.github.com>
2026-05-25 20:45:33 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,7 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
+    // 🟢 تم حذف إعدادات debugConfig من هنا 
   }
 
   buildTypes {
@@ -44,7 +39,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
+      // 🟢 تم مسح السطر الخاص بالمفتاح المفقود عشان يشتغل بالإعدادات الافتراضية
     }
   }
   
